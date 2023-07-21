5a4312deff92476da24efe1ce4294a0572691596
szutciek <szutermaciek@gmail.com>
2023-07-22 00:23:59 +0200

    Login Styled

--- client/js/WindowControllers/LoginPopup.js
+++ client/js/WindowControllers/LoginPopup.js
@@ -4,8 +4,8 @@ import Popup from "./Popup.js";
 
 export default class LoginPopup extends Popup {
   // Customizable variables
-  clamps = ["vh:50"];
-  defaultClamp = "vh:50";
+  clamps = ["px:382.5"];
+  defaultClamp = "px:382.5";
   userDraggable = false;
 
   constructor(elementId, controller) {
@@ -22,18 +22,16 @@ export default class LoginPopup extends Popup {
   }
 
   insertContent() {
-    const parent = this.element.querySelector(".code");
-    const width = parent.getBoundingClientRect().width;
+    const parent = this.element.querySelector(".codeBackground");
+    parent.innerHTML = "";
     const canvas = document.createElement("canvas");
     new QRious({
       element: canvas,
-      background: "#000",
-      backgroundAlpha: 0.1,
+      backgroundAlpha: 0,
       foreground: "#181818",
       foregroundAlpha: 1,
       level: "H",
-      padding: 16,
-      size: width,
+      size: 500,
       value: `${config.baseUrl}/authStation?stationId=${this.controller.stationId}`,
     });
     parent.appendChild(canvas);
