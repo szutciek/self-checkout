9c3f88201556f1db866cb455159ef168a47eab20
szutciek <szutermaciek@gmail.com>
2023-07-21 23:52:02 +0200

    QR code generation

--- client/js/WindowControllers/LoginPopup.js
+++ client/js/WindowControllers/LoginPopup.js
@@ -14,4 +14,26 @@ export default class LoginPopup extends Popup {
   handleOutsideClick() {
     if (this.visible && !this.inTransition) this.hide();
   }
+
+  showSpecific() {
+    this.insertContent();
+  }
+
+  insertContent() {
+    const parent = this.element.querySelector(".code");
+    const width = parent.getBoundingClientRect().width;
+    const canvas = document.createElement("canvas");
+    new QRious({
+      element: canvas,
+      background: "#000",
+      backgroundAlpha: 0.1,
+      foreground: "#181818",
+      foregroundAlpha: 1,
+      level: "H",
+      padding: 16,
+      size: width,
+      value: "/login",
+    });
+    parent.appendChild(canvas);
+  }
 }
