33a6667a805f2338ad3c45b01eadaf75e89e9fa0
szutciek <szutermaciek@gmail.com>
2023-07-22 23:28:14 +0200

    Temporary Login

--- client/js-u/script.js
+++ client/js-u/script.js
@@ -12,18 +12,14 @@ const ws = new WebSocket(config.wsUrl);
 
 ws.addEventListener("open", () => {
   console.log("Connection to server open");
-  document.body.style.backgroundColor = "green";
 });
 
 const usernameInput = document.getElementById("usernameInput");
 
 const authorizeButton = document.querySelector("#authorizeStation");
 authorizeButton.addEventListener("click", () => {
-  document.body.style.backgroundColor = "yellow";
-
   if (!stationId) return;
   if (usernameInput.value?.length > 3) user.name = usernameInput.value;
-  document.body.style.backgroundColor = "red";
 
   ws.send(JSON.stringify({ type: "authorizeStation", stationId, user }));
 });
